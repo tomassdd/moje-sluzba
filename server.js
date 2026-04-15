b113156605c15c970e001c7d05a0f80d0f69b98e
tomassdd <bezezubkatv@gmail.com>
2026-04-15 20:53:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post("/ai", async (req, res) => {
 	const prompt = req.body.prompt || "Ahoj";
 
 	try {
-		const response = await fetch("https://tomass.kurim.ithope.eu//v1/chat/completions", {
+		const response = await fetch("https://kurim.ithope.eu/v1/chat/completions", {
 		method: "POST",
 		headers: {
 		 "Content-Type": "application/json",
